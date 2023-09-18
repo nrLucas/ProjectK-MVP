a1ef5f07510f050eb0bcb32a4efa7274641e827c
nrLucas <lucasnrx@gmail.com>
2023-09-18 10:46:14 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+
+import { StyleSheet, View, Text } from "react-native";
+import {
+  loadTensorflowModel,
+  useTensorflowModel,
+} from "react-native-fast-tflite";
 
 export default function App() {
+  const [result, setResult] = React.useState("");
+
+  const model = useTensorflowModel(
+    require("../assets/object_detection_mobile_object_localizer_v1_1_default_1.tflite"),
+    "core-ml"
+  );
+
+  React.useEffect(() => {
+    if (model.model == null) return;
+
+    console.log(`Running Model...`);
+    const r = model.model.run([new Uint8Array([5])]);
+    r.then((output) => {
+      console.log(`Successfully ran Model!`, output);
+      setResult(`${output[0]}${output[1]}${output[2]}...`);
+    });
+  }, [model.model]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
     </View>
   );
 }
@@ -13,8 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
